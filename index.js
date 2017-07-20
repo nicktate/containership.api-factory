4aba317abe90cdf8679e9081bf7025f8a66673b9
Nicholas Tate <ntate22@gmail.com>
2017-07-20 09:00:22 -0400

    Added api factory

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const ContainershipApi = require('@containership/containership.api-bridge');
+const K8sApi = require('@containership/containership.k8s.api-bridge');
+
+const Constructor = process.env.CS_ORCHESTRATOR === 'kubernetes' ? K8sApi : ContainershipApi;
+const DEFAULT_PORT = 8080;
+
+module.exports = { Constructor, DEFAULT_PORT }
